6de2b19596dc47c93cf306da1ac5435aa2204273
Cristina Alba <Cristina.Jular.Alba@formandos.cinel.pt>
2021-10-01 10:21:57 +0100

    Changes in DB First Repositories

---
+++ Common/Data/Entities/Proposal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web_ManagementHouseRentals.Data;
+
+namespace Common.Data.Entities
+{
+    class Proposal
+    {
+        public int Id { get; set; }
+   
+        public Property property { get; set; }
+
+        public ProposalState ProposalState { get; set; }
+
+        public string Message { get; set; }
+    }
+}
--- Web_ManagementHouseRentals/Startup.cs
+++ Web_ManagementHouseRentals/Startup.cs
@@ -25,6 +25,9 @@ namespace Web_ManagementHouseRentals
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddControllersWithViews();
+
+            //Use the Seed the first time DB is executed
+            services.AddTransient<SeedDb>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

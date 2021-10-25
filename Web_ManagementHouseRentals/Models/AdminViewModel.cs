f83bf3bb91b3439f86955ab41b1937c0ae47f590
Cristina Alba <Cristina.Jular.Alba@formandos.cinel.pt>
2021-10-26 00:34:50 +0100

    Counter Dashboard

---
+++ Web_ManagementHouseRentals/Models/AdminViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_ManagementHouseRentals.Models
+{
+    public class AdminViewModel
+    {
+        public int TotalLandlords { get; set; }
+        public int TotalTenants { get; set; }
+        public int TotalWorkers { get; set; }
+        public int TotalProperties { get; set; }
+        
+
+    }
+}
